0dcafc49008720b6d1ad1f79fc383f86ccf1078e
itrinia <94580529+itrinia@users.noreply.github.com>
2023-04-26 20:03:14 +0700

    Initial Commit

---
+++ 0706012110019-ileenetrinia-AFL3-ucwalk/_706012110019_ileenetrinia_AFL3_ucwalkApp.swift
@@ -0,0 +1,17 @@
+//
+//  _706012110019_ileenetrinia_AFL3_ucwalkApp.swift
+//  0706012110019-ileenetrinia-AFL3-ucwalk
+//
+//  Created by MacBook Pro on 26/04/23.
+//
+
+import SwiftUI
+
+@main
+struct _706012110019_ileenetrinia_AFL3_ucwalkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
