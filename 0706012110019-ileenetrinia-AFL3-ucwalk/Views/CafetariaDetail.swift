33f760ed1013b7be4455bec093092fa1e1f2448d
itrinia <94580529+itrinia@users.noreply.github.com>
2023-04-27 16:36:59 +0700

    cafetaria detail line 37 still error

--- 0706012110019-ileenetrinia-AFL3-ucwalk/Views/CafetariaDetail.swift
+++ 0706012110019-ileenetrinia-AFL3-ucwalk/Views/CafetariaDetail.swift
@@ -15,16 +15,16 @@ struct CafetariaDetail: View {
     var cafetaria: Cafetaria
     var menu: Cafetaria.Menu
     
-//    var cafetariaIndex: Int {
-//        cafetariaData.cafetarias.firstIndex(where: { $0.id == cafetaria.id }) ?? 0
-//    }
+    var cafetariaIndex: Int {
+        cafetariaData.menus.firstIndex(where: { $0.id == menu.id }) ?? 0
+    }
+    
     
     var body: some View {
         ScrollView {
             Text(cafetaria.cafe)
             //loop2
             
-        }
         }
     }
 }
@@ -34,7 +34,7 @@ struct CafetariaDetail_Previews: PreviewProvider {
     static let cafetariaData = CafetariaData()
     
     static var previews: some View {
-        CafetariaDetail(cafetaria: CafetariaData().cafetarias[])
+        CafetariaDetail(cafetaria: CafetariaData().menus[])
             .environmentObject(cafetariaData)
     }
 }
